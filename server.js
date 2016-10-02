13adb64908297efab312e84e54a86ea0e637b381
akhilr163 <infrofriend1@gmail.com>
2016-10-02 16:55:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,24 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/article-one',function (req, res) { 
+    res.send('article one requested and will be served here');  
+});
+
+app.get('/article-two',function (req, res) { 
+    res.send('article one requested and will be served here');  
+});app.get('/ui/style.css', function (req, res) {
+  
+});  
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/article-three',function (req, res) { 
+    res.send('article one requested and will be served here');  
+});
+    
+app.get('/article-four',function (req, res) { 
+    res.send('article one requested and will be served here');  
+});
+    
 });
 
 app.get('/ui/madi.png', function (req, res) {
