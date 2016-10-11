b04eb119a54d1ad8bb02e4976befe41733289372
akhilr163 <infrofriend1@gmail.com>
2016-10-11 16:33:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req, res) { 
-   res.sendfile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.sendfile(path.join(_dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function (req, res) { 
